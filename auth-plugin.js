436cbfa441bea5f83375d270bc3b06686ca58dfd
Lydia910 <lydia911999@gmail.com>
2025-09-08 16:14:45 +1000

    Update auth-plugin.js

--- auth-plugin.js
+++ auth-plugin.js
@@ -14,7 +14,7 @@ function resolvePluginBase() {
 export const PLUGIN_BASE  = resolvePluginBase();
 export const PLUGIN_LOGIN = PLUGIN_BASE ? `${PLUGIN_BASE}/login.html` : null;
 export const PLUGIN_CHAT  = PLUGIN_BASE ? `${PLUGIN_BASE}/111.html`   : null;
-export const PLUGIN_ME    = PLUGIN_BASE ? `${PLUGIN_BASE}/me`         : null;
+export const PLUGIN_ME    = PLUGIN_BASE ? `${PLUGIN_BASE}/me.json`         : null;
 
 export function goToPluginLogin(next = "/chat.html") {
   if (!PLUGIN_BASE) {
