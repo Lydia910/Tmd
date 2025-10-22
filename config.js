848524f17e3e90ca3a2e32121fc8aaf2a6f3c4d5
Lydia910 <lydia911999@gmail.com>
2025-10-22 13:39:51 +1100

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-
-(function () {
-  var DEFAULT_BASE = 'https://617654bb26fa.ngrok-free.app/plugin';
-  if (!localStorage.getItem('tmd_plugin_base')) {
-    localStorage.setItem('tmd_plugin_base', DEFAULT_BASE);
-  }
-})();
